6daf74f8b3863155c7955839d4fc49dbd3b39904
ragimovalik <ragimovalik@gmail.com>
2021-09-01 15:58:05 +0300

    Start to configurate DB

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-const contactsRouter = require('./routes/api/contacts')
+const DB_HOST = require("./config");
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+app.use(cors());
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.get("/", (req, res) => {
+  res.send("<h2>Home page</h2>");
+});
 
-app.use('/api/contacts', contactsRouter)
-
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
-
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
-
-module.exports = app
+mongoose
+  .connect(DB_HOST, {
+    useNewURLParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connect success");
+    app.listen(4000);
+  })
+  .catch((error) => console.log(error));
