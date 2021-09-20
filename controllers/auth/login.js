c1d1085a2970768277853f06ec6355113d9970c8
ragimovalik <ragimovalik@gmail.com>
2021-09-20 22:29:45 +0300

    In progress - controllers updated

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,46 +1,29 @@
-const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
+const { BadRequest } = require("http-errors");
+const jwt = require("jsonwebtoken");
 const { User } = require("../../models");
 
-const login = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-    if (!user || !user.comparePassword(password)) {
-      return res.status(400).json({
-        status: "error",
-        code: 400,
-        message: "Wrong email or password",
-      });
-    }
-
-    /* if (!user) {
-      return res.status(400).json({
-        status: "error",
-        code: 400,
-        message: "Wrong email",
-      });
-    }
-
-    const hashPassword = user.password;
-    const compareResult = bcrypt.compareSync(password, hashPassword);
-
-    if (!compareResult) {
-      return res.status(400).json({
-        status: "error",
-        code: 400,
-        message: "Wrong password",
-      });
-    }
-    */
-    const token = "qweoiu.123asdasdasda.mnbzmnbzc";
-
-    res.json({
-      token,
-    });
-  } catch (error) {
-    next(error);
+  if (!user || !user.comparePassword(password)) {
+    throw new BadRequest("Wrong email or password");
   }
+
+  const payload = {
+    id: user._id,
+  };
+
+  const { SECRET_KEY } = process.env;
+
+  const token = jwt.sign(payload, SECRET_KEY);
+
+  await User.findByIdAndUpdate(user._id, { token });
+
+  res.json({
+    token,
+  });
 };
 
 module.exports = login;
